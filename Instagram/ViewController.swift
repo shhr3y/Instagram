142740d61e4570e0ceebe99e54b00bc0219ba894
Shrey <shhr3y@gmail.com>
2020-07-20 14:54:40 +0530

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Shrey Gupta on 20/07/20.
+//  Copyright © 2020 Shrey Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
